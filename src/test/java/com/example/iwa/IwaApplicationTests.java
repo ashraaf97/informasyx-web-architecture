7c9e0e88e29977c8d499888b54928058e1bd82eb
ash <axlrapid97@gmail.com>
2024-05-16 21:59:09 +0800

    initial

---
+++ src/test/java/com/example/iwa/IwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
